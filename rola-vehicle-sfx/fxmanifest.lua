0ccb9db5608a251aaf2a3f96895db042d40707ce
qkhu <qkhq321@gmail.com>
2024-06-30 17:27:15 -0500

    s

---
+++ rola-vehicle-sfx/fxmanifest.lua
@@ -0,0 +1,106 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+files {
+	'audioconfig/*.dat10',
+	'audioconfig/*.dat10.nametable',
+	'audioconfig/*.dat10.rel',
+	'audioconfig/*.dat151',
+	'audioconfig/*.dat151.nametable',
+	'audioconfig/*.dat151.rel',
+	'audioconfig/*.dat54',
+	'audioconfig/*.dat54.nametable',
+	'audioconfig/*.dat54.rel',
+	'sfx/**/*.awc',
+  }
+
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/s63b44_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/s63b44_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_s63b44'
+data_file "AUDIO_SYNTHDATA" "audioconfig/lg81hcredeye_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/lg81hcredeye_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/lg81hcredeye_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_lg81hcredeye"
+data_file 'AUDIO_GAMEDATA' 'audioconfig/aqm275amg_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/aqm275amg_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_aqm275amg'
+data_file "AUDIO_SYNTHDATA" "audioconfig/lg62chironpursport_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/lg62chironpursport_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/lg62chironpursport_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_lg62chironpursport"
+data_file "AUDIO_SYNTHDATA" "audioconfig/mbnzc63eng_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/mbnzc63eng_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/mbnzc63eng_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_mbnzc63eng"
+data_file 'AUDIO_GAMEDATA' 'audioconfig/gallardov10_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/gallardov10_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_gallardov10' 
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ta006bmws65_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ta006bmws65_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ta006bmws65'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/s85b50_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/s85b50_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_s85b50'
+data_file "AUDIO_SYNTHDATA" "audioconfig/c30a_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/c30a_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/c30a_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_c30a"
+data_file 'AUDIO_GAMEDATA' 'audioconfig/rb26dett_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/rb26dett_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_rb26dett'
+data_file "AUDIO_SYNTHDATA" "audioconfig/fdvsffeng_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/fdvsffeng_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/fdvsffeng_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_fdvsffeng"
+data_file 'AUDIO_GAMEDATA' 'audioconfig/aq56honf20c_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/aq56honf20c_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_aq56honf20c'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ta488f154_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ta488f154_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ta488f154'
+data_file "AUDIO_SYNTHDATA" "audioconfig/ml720v8eng_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/ml720v8eng_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/ml720v8eng_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_ml720v8eng"
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ta178amgb_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ta178amgb_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ta178amgb'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/chevroletlt4_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/chevroletlt4_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_chevroletlt4'
+data_file "AUDIO_SYNTHDATA" "audioconfig/lg124srt8thndrc_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/lg124srt8thndrc_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/lg124srt8thndrc_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_lg124srt8thndrc"
+data_file 'AUDIO_GAMEDATA' 'audioconfig/m158huayra_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/m158huayra_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_m158huayra'
+data_file "AUDIO_SYNTHDATA" "audioconfig/lg59hurv10_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/lg59hurv10_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/lg59hurv10_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_lg59hurv10"
+data_file 'AUDIO_GAMEDATA' 'audioconfig/tamustanggt50_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/tamustanggt50_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_tamustanggt50'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/tascmustanggt50_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/tascmustanggt50_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_tascmustanggt50'
+data_file "AUDIO_SYNTHDATA" "audioconfig/c30a_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/c30a_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/c30a_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_c30a"
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/nisgtr35_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/nisgtr35_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/nisgtr35_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_nisgtr35'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ta122s58_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ta122s58_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ta122s58'
+data_file "AUDIO_SYNTHDATA" "audioconfig/lg81hcredeye_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/lg81hcredeye_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/lg81hcredeye_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_lg81hcredeye"
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ta023l539_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ta023l539_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ta023l539'
