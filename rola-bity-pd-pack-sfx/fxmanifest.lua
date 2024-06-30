afec292ae9e2f75c84392527b96548bceaa6fc60
qkhu <qkhq321@gmail.com>
2024-06-30 17:28:23 -0500

    s

---
+++ rola-bity-pd-pack-sfx/fxmanifest.lua
@@ -0,0 +1,36 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+files {
+	'audioconfig/*.dat10',
+	'audioconfig/*.dat10.nametable',
+	'audioconfig/*.dat10.rel',
+	'audioconfig/*.dat151',
+	'audioconfig/*.dat151.nametable',
+	'audioconfig/*.dat151.rel',
+	'audioconfig/*.dat54',
+	'audioconfig/*.dat54.nametable',
+	'audioconfig/*.dat54.rel',
+	'sfx/**/*.awc',
+  }
+
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/npolchar_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/npolchar_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_npolchar'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ecoboostv6_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ecoboostv6_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ecoboostv6'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/cvpiv8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/cvpiv8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_cvpiv8'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/lg81hcredeye_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/lg81hcredeye_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_lg81hcredeye'
+
+
+
+
